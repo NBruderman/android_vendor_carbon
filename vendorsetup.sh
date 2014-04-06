23e33f34962ed85357b0bb83bf09602e2d9e86b9
Nir Bruderman <nbruderman@gmail.com>
2014-04-07 01:41:21 +0300

    Add Samsung GT-I9500 support

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -34,6 +34,7 @@ add_lunch_combo carbon_infuse4g-userdebug
 add_lunch_combo carbon_i9100-userdebug
 add_lunch_combo carbon_i9300-userdebug
 add_lunch_combo carbon_i9305-userdebug
+add_lunch_combo carbon_i9500-userdebug
 add_lunch_combo carbon_jflte-userdebug
 add_lunch_combo carbon_l900-userdebug
 add_lunch_combo carbon_ls980-userdebug
